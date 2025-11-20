666efb50645646a2cb6429f5faae8ca101fdf475
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2025-11-19 19:15:34 -0500

    desktop login v6

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { loadPlayerStatsAWS } from "./store/awsProfiles";
 import { useGameStore } from "./store/store";
 
 const GameCanvas = lazy(() => import("./components/GameCanvas"));
-const BYPASS_AUTH = (import.meta.env.VITE_BYPASS_AUTH ?? "false") === "false";
+const BYPASS_AUTH = (import.meta.env.VITE_BYPASS_AUTH ?? "false") === "true";
 
 function App() {
   const [user, setUser] = useState<SessionUser | null>(null);
